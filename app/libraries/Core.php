97ad207519b7e6006a4df74fb86d9f4dd656dc96
Suraj-Pahari <surajpahari152@gmail.com>
2022-09-23 14:58:41 +0545

    set current parameter for m url

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -3,33 +3,54 @@
 //Creates URL and loads core controller
 //URL Format - /controller/methods/param
 
-class Core{
+class Core
+{
     protected $current_controller = 'Pages';
     protected $current_method = 'index';
 
-    public function __construct(){
-       $url = $this->get_url();
+    public function __construct()
+    {
+        $url = $this->get_url();
 
         //look in controller for first  value
-    
-        if(file_exists('../app/controllers/'.ucwords($url[0]).'.php')){
-            $this->current_controller = ucwords($url[0]);
+        // as first value of the url is controller
+
+
+        if (isset($url)) {
+            if (file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+                //if controller exists set it the current_controller */
+                $this->current_controller = ucwords($url[0]);
+                //usets the url[0]
+                unset($url[0]);
+            }
+            //require the controller
+            require_once '../app/controllers/' . $this->current_controller . '.php';
+            $this->current_controller = new $this->current_controller;
         }
+        //check for the second part of the url 
+        if(isset($url[1])){
+            //check to see if method exists in controller
+            if(method_exists($this->current_controller,$url[1])){
+                $this->current_method = $url [1];
+            }
+        }
+
+        echo $this->current_method;
     }
 
 
     /*function to check url
     *separate url by '/' sign and filter url content
     *return them in the array*/
-    public function get_url(){
-        if(isset($_GET['url'])){
-            $url=rtrim($_GET['url'],'/');
+    public function get_url()
+    {
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
             //filters the illegal url characters
-            $url = filter_var($url,FILTER_SANITIZE_URL);
-            $url = explode('/',$url);
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
 
             return $url;
         }
     }
-
 }
