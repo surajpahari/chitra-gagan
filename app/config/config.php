c83d7da3e05c59f31f5cc75e609942a295667b80
Suraj-Pahari <surajpahari152@gmail.com>
2022-09-24 10:11:32 +0545

    defining some constant

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+//config files
+//declaring some constant
+//define APPROOT
+define('APPROOT',dirname(dirname(__FILE__)));
+//define URLROOT
+define('URLROOT','http://localhost/Chitra-Gagan');
+//define SITENAME
+define('SITENAME','ChitraGagan');
