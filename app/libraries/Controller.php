0fd25e168180eb315e656b62af2bab0874e12064
Suraj-Pahari <surajpahari152@gmail.com>
2022-09-24 09:53:51 +0545

    controller class for managing view and model

---
+++ app/libraries/Controller.php
@@ -0,0 +1,29 @@
+<?php
+/* Base Controller loads the models and views*/
+
+class Controller
+{
+    public function __construct()
+    {
+    }
+    public function model($model)
+    {
+        //require model file
+        require_once '..app/models/' . $model . '.php';
+
+        return new $model();
+    }
+
+    //load view
+    public function view($view, $data = [])
+    {
+        //check for the view file
+        if (file_exists('../app/views/' . $view . '.php')) {
+            require_once '../app/views/' . $view . '.php';
+        }
+        else{
+            //view doesnot exists
+            die('View does not exits');
+        }
+    }
+}
