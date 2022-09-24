047e0877cf6ee20c8d627df4475e8eca402ea7c2
Suraj-Pahari <surajpahari152@gmail.com>
2022-09-25 02:18:28 +0545

    added logout and session creator method

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -3,6 +3,7 @@ class Users extends Controller
 {
     public function __construct()
     {
+        $this->user_model = $this->model('User');
     }
 
     public function register()
@@ -38,16 +39,28 @@ class Users extends Controller
                 $data['fname_err'] = 'Please enter fname';
             }
             //validate lname
-            if (empty($data['lame'])) {
+            if (empty($data['lname'])) {
                 $data['lname_err'] = 'Please enter lname';
             }
             //validate email
             if (empty($data['email'])) {
                 $data['email_err'] = 'Please enter email';
+            } else {
+                //check email
+                if ($this->user_model->find_user_by_email($data['email'])) {
+                    echo $data['email'];
+                    $data['email_err'] = 'Email is already taken';
+                }
             }
             //validate username
             if (empty($data['username'])) {
                 $data['username_err'] = 'Please enter username';
+            } else {
+                //check email
+                if ($this->user_model->find_user_by_username($data['username'])) {
+                    echo $data['username'];
+                    $data['username_err'] = 'Username is already taken';
+                }
             }
             //validate Password 
             if (empty($data['password'])) {
@@ -68,9 +81,19 @@ class Users extends Controller
                 empty($data['lname_err']) && empty($data['username_err']) &&
                 empty($data['password_err']) && empty($data['confirm_password_err'])
             ) {
-                //validated 
-                die('Sucess');
+                //validated
+                //hash password
+                $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+
+                //register user
+                if ($this->user_model->register($data)) {
+                    flash('register_sucess', 'Registration Sucessful');
+                    redirect('users/login');
+                } else {
+                    die('Something went wrong');
+                }
             } else {
+                echo 'not validated';
                 print_r($data);
                 //load view with errors
                 // $this->view('users/register', $data);
@@ -112,18 +135,36 @@ class Users extends Controller
             if (empty($data['password'])) {
                 $data['password_err'] = 'Please enter password';
             }
+            //check for username
+            if ($this->user_model->find_user_by_username($data['username'])) {
+                //user found 
+            } else {
+                //user not found
+                $data['username_err'] = 'No user found';
+                //error
+            }
             if (
                 empty($data['username_err']) &&
-                empty($data['password_err'])){
-                //vlidated 
-                echo 'validated';
+                empty($data['password_err'])
+            ) {
+                //validated 
+                //check and set logged in user
+                $logged_in_user = $this->user_model->login($data['username'], $data['password']);
+
+                if ($logged_in_user) {
+                    //create session
+                    $this->create_user_session($logged_in_user);
+
+                } else {
+                    $data['password_err'] = 'Password Incorrect';
+                    $this->view('users/login', $data);
+                }
             } else {
-                echo 'validated';
+                echo 'not validated';
+                print_r($data);
                 //load view with errors
                 // $this->view('users/register', $data);
             }
-
-
         } else {
             //Init data
             $data = [
@@ -135,5 +176,25 @@ class Users extends Controller
             ];
             $this->view('users/login', $data);
         }
+    }
+    public function create_user_session($user)
+    {
+        $_SESSION['user_id'] = $user->id;
+        $_SESSION['user_fname'] = $user->fname;
+        $_SESSION['user_lname'] = $user->lname;
+        $_SESSION['user_username'] = $user->username;
+        $_SESSION['user_email'] = $user->email;
+        echo 'welcome '. $_SESSION['user_fname'];
+
+    }
+    public function logout()
+    {
+        unset($_SESSION['user_id']);
+        unset($_SESSION['user_fname'] );
+        unset($_SESSION['user_lname']);
+        unset($_SESSION['user_username']);
+        unset($_SESSION['user_email']);
+        session_destroy();
+        redirect('users/login');
     }
 }
