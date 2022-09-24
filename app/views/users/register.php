c465bc434cea5dc58b670794cfd3d540df906c51
Suraj-Pahari <surajpahari152@gmail.com>
2022-09-24 22:04:36 +0545

    created register form

---
+++ app/views/users/register.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Signup Form</title>
+  <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
+</head>
+
+<body>
+  <div id="bg"></div>
+  <form action="<?php echo URLROOT; ?>/users/register" method="post">
+    <div class="formContainer">
+      <h1>Sign Up </h1>
+
+      <label for="First Name"><b>First Name</b></label>
+      <input type="text" placeholder="Enter First Name" name="First Name" value="<?php echo $data['fname'] ?>" required>
+
+      <label for="Last  Name"><b>Last Name</b></label>
+      <input type="text" placeholder="Enter Last Name" name="Last Name" value="<?php echo $data['lname'] ?>" required>
+      <label for="email"><b>Email</b></label>
+      <input type="text" placeholder="Enter Email" name="email" value="<?php echo $data['email'] ?>" required>
+      <label for="email"><b>Email</b></label>
+      <input type="text" placeholder="Enter Username" name="username" value="<?php echo $data['username'] ?>" required>
+      <label for="password"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="password" value="<?php echo $data['password'] ?>" required>
+      <label for="ConfirmPassword"><b>Confirm Password</b></label>
+      <input type="password" placeholder="Confirm Password" name="Confirm Password" value="<?php echo $data['confirm_password'] ?>" required>
+
+      <div>
+        <button type="button" class="login"><a href="<?php echo URLROOT; ?>/users/login">Log in</a></button>
+        <button type="submit" class="signup">Sign Up</button>
+      </div>
+    </div>
+  </form>
+</body>
+
+</html>
