9dbff1cc22afb62cac97ad944c9f1a40e019298f
Suraj-Pahari <surajpahari152@gmail.com>
2022-09-24 11:01:24 +0545

    define database param

--- app/config/config.php
+++ app/config/config.php
@@ -1,6 +1,11 @@
 <?php
 //config files
 //declaring some constant
+//defining some param for database
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','underside');
+define('DB_NAME','chitra_gagan');
 //define APPROOT
 define('APPROOT',dirname(dirname(__FILE__)));
 //define URLROOT
