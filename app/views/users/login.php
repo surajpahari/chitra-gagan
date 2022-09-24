7b36e86b54b5ce28736abd65e411e6cf8f12083d
Suraj-Pahari <surajpahari152@gmail.com>
2022-09-24 22:04:11 +0545

    created  login form

---
+++ app/views/users/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Log in</title>
+  <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
+ </head>
+
+<body>
+  <div id="bg"></div>
+  <form action="<?php echo URLROOT; ?>/users/login" method="post">
+    <div class="formContainer">
+      <h1>Log in</h1>
+
+       
+      <label for="username"><b>Email</b></label>
+      <input type="text" placeholder="Enter username" name="username" value="<?php echo $data['username']?>" required>
+      <label for="password"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="password" value="<?php echo $data['password'] ?>" required>
+       
+
+      <div>
+        <button type="button" class="login"><a href="<?php echo URLROOT; ?>/users/register">Sign in</a></button>
+        <button type="submit" class="signup">Log in</button>
+      </div>
+    </div>
+  </form>
+</body>
+
+</html>
