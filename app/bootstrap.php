4b6eb6e7e3caa6ed7900fbe3f68a4e5ba5c1c34e
Suraj-Pahari <surajpahari152@gmail.com>
2022-09-25 07:53:35 +0545

    added some  helper to include'

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,9 @@
 <?php
 //loads the require library
  require_once 'config/config.php';
+ //load helper
+ require_once 'helpers/url_helper.php';
+ require_once 'helpers/session_helper.php';
 //autoload all the required library
 spl_autoload_register(function($class_name){
     require_once('libraries/'. $class_name . '.php');
