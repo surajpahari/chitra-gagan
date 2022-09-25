d0a691106d362f22acf117852acb0292fc2f86c1
Suraj-Pahari <surajpahari152@gmail.com>
2022-09-25 08:00:35 +0545

    test

---
+++ app/controllers/Pages.php
@@ -0,0 +1,23 @@
+<?php
+class Pages extends Controller
+{
+    public function __construct()
+    {
+    
+    }
+
+    public function index()
+    {
+         $data = [
+            'title' => 'Welcome'
+        ];
+        $this->view('pages/index', $data);
+    }
+    public function about()
+    {
+        $data = [
+            'title' => 'About'
+        ];
+        $this->view('pages/about', $data);
+    }
+}
