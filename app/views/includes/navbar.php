a5950c898ecb004e34e4777831782bdb6edff692
Suraj-Pahari <surajpahari152@gmail.com>
2022-11-09 11:10:11 +0545

    added dropdown

--- app/views/includes/navbar.php
+++ app/views/includes/navbar.php
@@ -5,10 +5,18 @@
     </h2>
     <div class="nav__profile">
       <?php if (isset($_SESSION['user_id'])) : ?>
-        <div class="nav__profile-image">
+        <div class="dropdown">
+          <div class="nav__profile-image">
           <img src="<?php echo URLROOT; ?>/images/avatar.jpg" alt="avatar" />
+          </div>
+          <div class="dropdown-content">
+            <a href="<?php echo URLROOT . '/pages' ?>">Home</a>
+            <a href="<?php echo URLROOT . '/images/upload' ?>">upload</a>
+            <a href="<?php echo URLROOT . '/pages/mygallery' ?>">My content</a>
+            <a href="<?php echo URLROOT . '/images/profile_upload'?>">Edit profile</a>
+          </div>
         </div>
-        <div class="nav__profile-options">
+          <div class="nav__profile-options">
           <a href="<?php echo URLROOT ?>/users/logout" class="options">
             <span><i class="uil uil-sign-out-alt"></i></span>
           </a>
